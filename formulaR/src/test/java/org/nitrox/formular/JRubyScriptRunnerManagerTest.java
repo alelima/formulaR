435a3c66f7b1838a2ab453e88b3fdb04cec3329e
Alessandro Lima <03883182443@serpro-1536637>
2013-08-09 19:02:08 -0300

    Changes because JrubyScriptRunnerManager

--- formulaR/src/test/java/org/nitrox/formular/JRubyScriptRunnerManagerTest.java
+++ formulaR/src/test/java/org/nitrox/formular/JRubyScriptRunnerManagerTest.java
@@ -4,7 +4,13 @@
  */
 package org.nitrox.formular;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import java.io.File;
 import java.math.BigDecimal;
+import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import javax.script.SimpleBindings;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -42,8 +48,8 @@ public class JRubyScriptRunnerManagerTest {
     /**
      * Test of evalRuby method, of class JRubyScriptRunnerManager.
      */
-    @Test
-    public void testEvalRuby() throws Exception {
+    //@Test
+   /* public void testEvalRuby() throws Exception {
         System.out.println("evalRuby");
         BigDecimal valor1 = new BigDecimal("20");
         BigDecimal valor2 = new BigDecimal("10");
@@ -58,6 +64,35 @@ public class JRubyScriptRunnerManagerTest {
         Object result = instance.evalRuby(valor1, valor2);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");*/
+        fail("The test case is a prototype.");
+    } */
+    
+    //@Test
+   /* public void testRunner() throws Exception {
+        JRubyScriptRunnerManager ruby = new JRubyScriptRunnerManager();
+        ScriptEngine engine = ruby.getEngine();
+        
+        BigDecimal valor1 = new BigDecimal("20.0000000007");
+        BigDecimal valor2 = new BigDecimal("10.0000089765");        
+        Bindings bindings = new SimpleBindings();
+        bindings.put("valor1", valor1);
+        bindings.put("valor2", valor2);
+        
+        System.out.println("ANTES DA OPERACAO: ");
+        System.out.println(System.currentTimeMillis());
+        BigDecimal resultado1 = (BigDecimal) engine.eval(ruby.getDefautScript1() + "valor1 - valor2 + sqrt(4) + 2", bindings);
+        System.out.println(System.currentTimeMillis());
+        //BigDecimal resultado2 = (BigDecimal) engine.eval(ruby.getDefautScript2() + "valor1 - valor2 + sqrt(4) + 2", bindings);
+                        
+        System.out.println("Resultado 1: " + resultado1);
+        //System.out.println("Resultado 2: " + resultado2);
+        
+        
+    } */
+    
+    @Test
+    public void testRunner2() throws Exception {
+        JRubyScriptRunnerManager jruby = new JRubyScriptRunnerManager();
+        jruby.eval();        
     }
 }
