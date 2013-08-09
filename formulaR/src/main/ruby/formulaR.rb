aba0bce0990d418798281b4c1d7dcc0270664a5f
Alessandro Lima <03883182443@serpro-1536637>
2013-08-09 19:01:27 -0300

    Simplification of ruby operations script

--- formulaR/src/main/ruby/formulaR.rb
+++ formulaR/src/main/ruby/formulaR.rb
@@ -1,44 +1,20 @@
-# here operations of numeric types of java, 
-# can be replaced by a best matematic formule style
-# and another operations and syntax can be created
-# as you wish
 
-#Redefine BigDecimal operations  
 class Java::JavaMath::BigDecimal
   
   def + (operand)
-    execute_operation(operand, "add")
+    self.add(operand);
   end
   
   def - (operand)
-    execute_operation(operand, "subtract")
+    self.subtract(operand);
   end
   
   def * (operand)
-    execute_operation(operand, "multiply")      
+    self.multiply(operand);      
   end
       
   def / (operand)
-    execute_operation(operand, "divide")       
-  end
-  
-  def sqrt(value)
-    Math.sqrt(value)
-  end
-  
-  def execute_operation (operand, java_operation)      
-    if operand.kind_of?(Numeric)
-      eval("self.#{java_operation}(Java::JavaMath::BigDecimal.new(#{operand}))") 
-    elsif operand.is_a?(Java::JavaMath::BigDecimal)
-      eval("self.#{java_operation}(#{operand})") 
-    end  
+    self.divide(operand);      
   end
     
-  def method_missing(name, *args)
-    puts "The method #{name} doesn't exist in BigDecimal Java Class"
-  end    
-end
-
-def resolveFormula formula
-  formula
 end
