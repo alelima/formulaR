6088e1fdc7dbfd8e94aa974845b828174a88712b
Alessandro Lima <03883182443@serpro-1536637>
2013-08-12 17:33:53 -0300

    Add equals method to Variable

--- formulaR/src/main/java/org/nitrox/formular/Variable.java
+++ formulaR/src/main/java/org/nitrox/formular/Variable.java
@@ -33,4 +33,26 @@ class Variable {
         this.value = value;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + (this.name != null ? this.name.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Variable other = (Variable) obj;
+        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
+
 }
