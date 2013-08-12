d7891de2e363f639b4a89e68bcdaf2045868b3d1
Alessandro Lima <03883182443@serpro-1536637>
2013-08-12 17:36:47 -0300

    More test behaviors add

--- formulaR/src/test/java/org/nitrox/formular/FormularTest.java
+++ formulaR/src/test/java/org/nitrox/formular/FormularTest.java
@@ -7,8 +7,10 @@ package org.nitrox.formular;
 
 import com.google.common.base.Splitter;
 import java.math.BigDecimal;
+import java.util.HashSet;
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.nitrox.formular.exception.NotValidExpressionException;
 
 /**
  *
@@ -129,6 +131,44 @@ public class FormularTest {
        System.out.println("EXPRESSAO : variable2 + variable3 +");
        assertFalse(formula.isValidForPattern("variable2 + variable3 log variable4 ln variable6 ", pattern));
         
+    }
+    
+    @Test
+    public void shouldReturnTheVariablesOfFormulaExpression() {
+       VariableBuild varBuild = new VariableBuild();
+       Variable var1 =  varBuild.withNameAndWithValue("var1", BigDecimal.ZERO).build();
+       Variable var2 =  varBuild.withNameAndWithValue("var2", BigDecimal.ZERO).build();
+       Variable var3 =  varBuild.withNameAndWithValue("var3", BigDecimal.ZERO).build();
+       Variable var4 =  varBuild.withNameAndWithValue("var4", BigDecimal.ZERO).build();
+       Variable var5 =  varBuild.withNameAndWithValue("var5", BigDecimal.ZERO).build();
+       
+       HashSet<Variable> variables = new HashSet<Variable>();
+       variables.add(var1);
+       variables.add(var2);
+       variables.add(var3);
+       variables.add(var4);
+       variables.add(var5);
+       
+       String expression = "var1 + var2 - var3 * var4 / var5";
+       
+       Formula formula = new Formula(); 
+       HashSet<Variable> result = formula.getExpressionVariables(expression);
+       
+       assertEquals(variables.size(), result.size());
+       
+        for (Variable variable : result) {
+            assertTrue(variables.contains(variable));
+        }        
+    }
+    
+    @Test (expected = NotValidExpressionException.class)
+    public void shouldThowExceptionWhenTryToGetVariablesOfFormulaExpression() {
+            
+       String expression = "var1 + var2 var3 * var4 / var5";
+       
+       Formula formula = new Formula(); 
+       HashSet<Variable> result = formula.getExpressionVariables(expression);
+              
     }
     
 }
