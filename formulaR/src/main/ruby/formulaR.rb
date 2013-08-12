fdc5ef2c362b20c2e76f33dfd746f1f0701302db
Alessandro Lima <03883182443@serpro-1536637>
2013-08-12 17:41:24 -0300

    Fixed problems with no BigDecimal numbers on expression

--- formulaR/src/main/ruby/formulaR.rb
+++ formulaR/src/main/ruby/formulaR.rb
@@ -2,19 +2,26 @@
 class Java::JavaMath::BigDecimal
   
   def + (operand)
-    self.add(operand);
+    self.add(convertToBigDecimal operand);
   end
   
   def - (operand)
-    self.subtract(operand);
+    self.subtract(convertToBigDecimal operand);
   end
   
   def * (operand)
-    self.multiply(operand);      
+    self.multiply(convertToBigDecimal operand);      
   end
       
   def / (operand)
-    self.divide(operand);      
+    self.divide(convertToBigDecimal(operand), Java::JavaMath::RoundingMode::HALF_EVEN);      
   end
     
+  def convertToBigDecimal number
+    if not number.is_a?(Java::JavaMath::BigDecimal)
+      number = Java::JavaMath::BigDecimal.new(number.to_s)
+    end 
+    number
+  end
+  
 end
